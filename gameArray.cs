cbb3c682dc037634b2f39ce3632562b0d585af1b
Chris Kapusta <ckapusta@outlook.com>
2022-11-15 14:48:57 -0600

    Added constructor logic

--- gameArray.cs
+++ gameArray.cs
@@ -7,6 +7,20 @@ namespace Consodle
         public gameArray()
         {
 
+            int uBound0 = gameBoard.GetUpperBound(0);
+            int uBound1 = gameBoard.GetUpperBound(1);
+
+            for (int i = 0; i <= uBound0; i++)
+            {
+                for (int j = 0; j <= uBound1; j++)
+                {
+                    gameBoard[i, j] = " ";
+
+                }
+            }
+
+
+
         }
 
         public void clearArray()
@@ -15,5 +29,34 @@ namespace Consodle
 
 
         }
+
+        public string[,] addGuess(int guessNum, string playerGuess)
+        {
+            for (int i = 0; i < playerGuess.Length; i++)
+            {
+                gameBoard[guessNum, i] = playerGuess[i].ToString();
+            }
+
+
+            return gameBoard;
+        }
+
+        public void printGameBoard()
+        {
+            int uBound0 = gameBoard.GetUpperBound(0);
+            int uBound1 = gameBoard.GetUpperBound(1);
+
+            for (int i = 0; i <= uBound0; i++)
+            {
+                for (int j = 0; j <= uBound1; j++)
+                {
+                    string res = gameBoard[i, j];
+                    Console.Write(res);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
     }
 }
