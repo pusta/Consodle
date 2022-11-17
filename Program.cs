3f8cc5479a81d83108077422fa9cebfc3708f907
Chris Kapusta <ckapusta@outlook.com>
2022-11-16 23:05:22 -0600

    Added error checking to guess

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
 
 namespace Consodle
 {
@@ -7,14 +8,36 @@ namespace Consodle
         private static void Main(string[] args)
         {
 
-
+            int guessNum = 0;
+            string playerGuess;
+            string fileName = @"wordlist.txt";
             selectWord selectedWord = new selectWord();
             var todayWord = selectedWord.getWord();
             gameArray gameBoard = new gameArray();
 
 
             Console.WriteLine(todayWord);
-            gameBoard.printGameBoard();
+            //gameBoard.printGameBoard();
+            while (guessNum <= 5)
+            {
+
+                Console.Write("Please enter a guess: ");
+                playerGuess = Console.ReadLine()!.ToLower();
+
+                while (playerGuess.Length != 5 || String.IsNullOrEmpty(playerGuess) || !File.ReadAllText(fileName).Contains(playerGuess))
+                {
+                    Console.Write("Invalid Guess, please try again:");
+                    playerGuess = Console.ReadLine()!;
+                }
+
+
+                gameBoard.addGuess(guessNum, playerGuess);
+                gameBoard.printGameBoard();
+                guessNum++;
+
+
+            }
+
 
 
         }
